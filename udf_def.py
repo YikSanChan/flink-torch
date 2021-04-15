9e2ef61556213458309f64a7118f2c6d9f3da63c
Yik San Chan <evan.chanyiksan@gmail.com>
2021-04-15 11:23:05 +0800

    Use model.eval() and torch.no_grad() on inference

--- udf_def.py
+++ udf_def.py
@@ -12,4 +12,7 @@ def predict(users, items):
     n_users, n_items = 943, 1682
     model = MatrixFactorization(n_users, n_items)
     model.load_state_dict(torch.load("model.pth"))
-    return pd.Series(model(users, items).detach().numpy())
+    model.eval()
+    with torch.no_grad():
+        preds = model(users, items)
+        return pd.Series(preds)
