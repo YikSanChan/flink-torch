76bc145de965b4b9cd42cea63af33d1b4d496b84
Yik San Chan <evan.chanyiksan@gmail.com>
2021-04-15 10:53:56 +0800

    Fix predict udf and pass test

--- test_udf_predict.py
+++ test_udf_predict.py
@@ -1,7 +1,12 @@
+import pandas as pd
+
 from udf_def import predict
 
 
 def test_predict():
     f = predict._func
-    pred = f(1, 2)
-    assert type(pred) is float
+    users = pd.Series([1, 2, 3])
+    items = pd.Series([1, 4, 9])
+    preds = f(users, items)
+    assert isinstance(preds, pd.Series)
+    assert len(preds) == 3
--- udf_def.py
+++ udf_def.py
@@ -2,14 +2,14 @@ from pyflink.table.udf import udf
 from pyflink.table import DataTypes
 
 import torch
+import pandas as pd
 
 from model_def import MatrixFactorization
 
 
-@udf(result_type=DataTypes.DOUBLE())
-def predict(user, item):
+@udf(result_type=DataTypes.DOUBLE(), func_type="pandas")
+def predict(users, items):
     n_users, n_items = 943, 1682
     model = MatrixFactorization(n_users, n_items)
     model.load_state_dict(torch.load("model.pth"))
-    # print(model)
-    return model([user], [item]).item()
+    return pd.Series(model(users, items).detach().numpy())
